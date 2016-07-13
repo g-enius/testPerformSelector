1809c7e61780ea3efa4be4bd3c792e3460f5e9ce
wangchao <wang_chao@corp.netease.com>
2016-07-13 11:07:18 +0800

    Initial Commit

---
+++ testPerformSelector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testPerformSelector
+//
+//  Created by Charles on 7/13/16.
+//  Copyright © 2016 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
